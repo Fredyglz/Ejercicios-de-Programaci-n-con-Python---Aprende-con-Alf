c5ccd7c179ddeee2eb44ef73aa3f40bf9e0bd476
Fredyglz <fredyglz.fr@gmail.com>
2020-11-18 19:06:40 -0600

    carpetas

---
+++ 06_Funciones/01_BuclesPython.py
@@ -0,0 +1,10 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 18/11/2020
+palabra = input("Ingrese una palabra: \n")
+for i in range(10):
+    print(palabra)
+
+# SOLUCION DE https://aprendeconalf.es/
+word = input("Introduce una palabra: ")
+for i in range(10):
+    print(word)
