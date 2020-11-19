1059bb226c513f92dd5d9f2e8659878074cc9dd7
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:24:09 -0600

    Create 01_CondicionalesPython.py

---
+++ 02_Condicionales/01_CondicionalesPython.py
@@ -0,0 +1,14 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 12/10/2020
+edad = int(input("Ingresa tu edad: \n"))
+if(edad < 18):
+    print("Eres menor de edad")
+else:
+    print("Eres mayor de edad")
+    
+# SOLUCION DE https://aprendeconalf.es/
+age = int(input("¿Cuál es tu edad? "))
+if age < 18: 
+    print ("Eres menor de edad.")
+else:
+    print("Eres mayor de edad.")
