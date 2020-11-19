a27348e0505adc8d459c043c534276b9b5fb1423
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 08:46:39 -0600

    Create 02_BuclesPython.py

---
+++ 03_Bucles/02_BuclesPython.py
@@ -0,0 +1,10 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 18/11/2020
+edad = int(input("Ingrese su edad: \n"))
+for i in range(edad):
+    print("Has cumplido", i+1, "años")
+    
+# SOLUCION DE https://aprendeconalf.es/
+age = int(input("¿Cuántos años tienes? "))
+for i in range(age):
+    print("Has cumplido " + str(i+1) + " años")
