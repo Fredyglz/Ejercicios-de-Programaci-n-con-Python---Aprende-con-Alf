3ee5a629cc6a921c2698fd8e371f56d7dc8f97ed
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 08:49:40 -0600

    Create 07_BuclesPython.py

---
+++ 03_Bucles/07_BuclesPython.py
@@ -0,0 +1,12 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 18/11/2020
+for i in range(1,11):
+    for j in range(1,11):
+        print(i, "x", j, "=", i*j)
+    print("\n")
+    
+# SOLUCION DE https://aprendeconalf.es/
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(i*j, end="\t")
+    print("")
