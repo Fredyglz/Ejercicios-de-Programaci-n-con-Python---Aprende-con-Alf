debf362c4a7a4e786c6eb8ee03578ca9b51730bf
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:28:13 -0600

    Create 04_CondicionalesPython.py

---
+++ 02_Condicionales/04_CondicionalesPython.py
@@ -0,0 +1,15 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 12/10/2020
+num = int(input("Ingrese un numero: \n"))
+
+if(num % 2 == 0):
+    print("Es par.")
+else:
+    print("Es impar")
+    
+# SOLUCION DE https://aprendeconalf.es/
+n = int(input("Introduce un número entero: "))
+if n % 2 == 0:
+    print("El número " + str(n) + " es par")
+else:
+    print("El número " + str(n) + " es impar")
