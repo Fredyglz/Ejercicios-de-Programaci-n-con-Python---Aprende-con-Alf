de37ee939ad84a327297bc1010ceddc38371ea14
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:01:04 -0600

    Create 04_TiposDatosSimplesPython.py

---
+++ 01_Tipos_de_Datos_Simples/04_TiposDatosSimplesPython.py
@@ -0,0 +1,11 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 08/10/2020
+nombre = input("Ingresa tu nombre: ")
+numero = int(input("Ingresa un numero entero: "))
+
+print((nombre + "\n") * numero)
+
+# SOLUCION  DE https://aprendeconalf.es/
+name = input("¿Cómo te llamas? ")
+n = input("Introduce un número entero: ")
+print((name + "\n") * int(n))
