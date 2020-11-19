06a439d98d4b5271bd90938d65c1624e1f36adac
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 08:47:43 -0600

    Create 04_BuclesPython.py

---
+++ 03_Bucles/04_BuclesPython.py
@@ -0,0 +1,10 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 18/11/2020
+num = int(input("Ingrese un numero: \n"))
+for i in range(num+1):
+    print(num-i, end=", ")
+    
+# SOLUCION DE https://aprendeconalf.es/
+n = int(input("Introduce un número entero positivo: "))
+for i in range(n, -1, -1):
+    print(i, end=", ")
