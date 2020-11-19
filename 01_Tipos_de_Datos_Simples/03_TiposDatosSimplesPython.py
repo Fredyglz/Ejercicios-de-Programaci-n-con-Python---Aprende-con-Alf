2e6f9bdca32c2b57c6d1e5aa200e6b91e70f7789
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:00:03 -0600

    Create 03_TiposDatosSimplesPython.py

---
+++ 01_Tipos_de_Datos_Simples/03_TiposDatosSimplesPython.py
@@ -0,0 +1,8 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 08/10/2020
+nombre = input("Ingresa tu nombre: ")
+print("¡Hola " + nombre + "!")
+
+# SOLUCION  DE https://aprendeconalf.es/
+name = input("Introduce tu nombre: ")
+print("¡Hola " + name + "!")
