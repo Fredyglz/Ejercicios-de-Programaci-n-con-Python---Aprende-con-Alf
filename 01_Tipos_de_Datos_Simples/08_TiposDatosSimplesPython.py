bb7eee6b73a3c1e61fcab25e585e97615bb9a5fe
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:06:41 -0600

    Create 08_TiposDatosSimplesPython.py

---
+++ 01_Tipos_de_Datos_Simples/08_TiposDatosSimplesPython.py
@@ -0,0 +1,11 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 08/10/2020
+n = int(input("Ingresa un numero entero positivo: \n"))
+suma = (n*(n+1))//2
+
+print(suma)
+
+# SOLUCION  DE https://aprendeconalf.es/
+n = int(input("Introduce un número entero: "))
+suma = n * (n + 1) / 2
+print("La suma de los primeros números enteros desde 1 hasta " + str(n) + " es " + str(suma))
