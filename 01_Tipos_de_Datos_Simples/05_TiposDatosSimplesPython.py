388437ed1e1fb6d88354c427772f80f4da0d5398
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:02:12 -0600

    Create 05_TiposDatosSimplesPython.py

---
+++ 01_Tipos_de_Datos_Simples/05_TiposDatosSimplesPython.py
@@ -0,0 +1,8 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 08/10/2020
+nombre = input("Ingresa tu nombre: \n")
+print(nombre.upper() + " tiene " + str(len(nombre)) + " letras.")
+
+# SOLUCION  DE https://aprendeconalf.es/
+name = input("¿Cómo te llamas? ")
+print(name.upper() + " tiene " + str(len(name)) + " letras")
