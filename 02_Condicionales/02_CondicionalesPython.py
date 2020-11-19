8f6dfc5fc9dec8457c6bbb0a43e457edc2b57055
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 09:26:13 -0600

    Create 02_CondicionalesPython.py

---
+++ 02_Condicionales/02_CondicionalesPython.py
@@ -0,0 +1,17 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 12/10/2020
+password = "contraseña"
+repPassword = input("Ingrese la contraseña: \n")
+
+if password == repPassword.lower():
+    print("La contraseña es correcta.")
+else:
+    print("La contraseña es incorrecta")
+    
+# SOLUCION DE https://aprendeconalf.es/
+key = "contraseña"
+password = input("Introduce la contraseña: ")
+if key == password.lower():
+    print("La contaseña coincide")
+else:
+    print("La contraseña no coincide")
