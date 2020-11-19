9eb3c49c7b08722fa35b8cb4cdbbf13b38e34eaa
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-19 08:50:46 -0600

    Create 09_BuclesPython.py

---
+++ 03_Bucles/09_BuclesPython.py
@@ -0,0 +1,15 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 18/11/2020
+password = 'contraseña'
+userPassword = input("Ingrese la contraseña: \n")
+while userPassword != password:
+    print("Contraseña incorrecta!!")
+    userPassword = input("Ingrese de nuevo la contraseña: \n")
+print("Contraseña correcta :D")    
+
+# SOLUCION DE https://aprendeconalf.es/
+key = "contraseña"
+password =""
+while password != key:
+    password = input("Introduce la contraseña: ")
+print("Contraseña correcta")
