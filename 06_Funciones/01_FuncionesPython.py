8f84c8685905a3d1fbe3cf503ff9bf281926b89b
Fredy Romero Gonzalez <44275036+Fredyglz@users.noreply.github.com>
2020-11-26 14:12:31 -0600

    Create 01_FuncionesPython.py

---
+++ 06_Funciones/01_FuncionesPython.py
@@ -0,0 +1,14 @@
+# JOSE ALFREDO ROMERO GONZALEZ 
+# 26/11/2020
+def saludo():
+    print("¡Hola amiga!")
+
+saludo()
+
+# SOLUCION DE https://aprendeconalf.es/
+def greet():
+    """Función que muestra el saluco ¡Hola amiga! por pantalla."""
+    print('¡Hola amiga!')
+    return
+
+greet()
